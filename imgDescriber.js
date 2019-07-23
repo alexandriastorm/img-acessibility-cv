a5b900a4915ff7f9b5cb3ee765277805b62ec209
Alexandria Storm <alexandriastorm99@gmail.com>
2019-07-22 17:50:03 -0700

    Basic Image Showing

--- imgDescriber.js
+++ imgDescriber.js
@@ -18,4 +18,21 @@ function getImageDescription() {
   }
 
   http.send("{\"url\":\"" + imgLink + "\"}" );
+ 
+ 
+  function add_google_logo() {
+    var src = imgLink;
+    show_image(src, 276,270, "Google Logo");
 }
+
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+    document.body.appendChild(img);
+}
+add_google_logo()
+}
+
